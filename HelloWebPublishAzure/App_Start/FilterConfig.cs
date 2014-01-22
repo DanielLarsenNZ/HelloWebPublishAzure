5324589055584c09973f08f722b3fe0ba2d8d3f8
DanielLarsenNZ <dlarsen@spendvision.com>
2014-01-22 17:10:53 +1300

    Initial commit

---
+++ HelloWebPublishAzure/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWebPublishAzure
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
